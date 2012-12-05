77f3a231fbf92c51879311bce8d2ad6a319a461f
Jacek Kobus <jacek.kobus@nestia.pl>
2012-12-05 22:49:42 +0100

    poprawienie metod klasy armor

--- src/com/stirante/PrettyScaryLib/Armor.java
+++ src/com/stirante/PrettyScaryLib/Armor.java
@@ -1,11 +1,10 @@
 package com.stirante.PrettyScaryLib;
 
-import java.awt.Color;
-
 import net.minecraft.server.NBTTagCompound;
-
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
+
+import java.awt.*;
 
 /**
  * Class, that allows setting and getting color of the leather armor.
@@ -14,43 +13,46 @@ public class Armor {
 
 	/**
 	 * Sets the color.
+	 * If item is not colorable it will return an unchanged object of that item.
 	 *
-	 * @param item item to color
+	 * @param item  item to color
 	 * @param color color
 	 * @return colored item
-	 * @throws Exception thrown,  when item is not applicable
 	 */
 	public static ItemStack setColor(ItemStack item, int color) {
+
 		if (!isApplicable(item))
-			return null;
+			return item;
+
 		CraftItemStack craftStack = null;
 		net.minecraft.server.ItemStack itemStack = null;
+
 		if (item instanceof CraftItemStack) {
 			craftStack = (CraftItemStack) item;
 			itemStack = craftStack.getHandle();
-		}
-		else if (item instanceof ItemStack) {
+		} else {
 			craftStack = new CraftItemStack(item);
 			itemStack = craftStack.getHandle();
 		}
+
 		NBTTagCompound tag = itemStack.tag;
 		if (tag == null) {
 			tag = new NBTTagCompound();
 			tag.setCompound("display", new NBTTagCompound());
 			itemStack.tag = tag;
 		}
-		
+
 		tag = itemStack.tag.getCompound("display");
 		tag.setInt("color", color);
 		itemStack.tag.setCompound("display", tag);
 		return craftStack;
 	}
-	
+
 	/**
-	 * Checks if item is applicable.
+	 * Check if item is applicable (colorable).
 	 *
-	 * @param item the item to check
-	 * @return true, if is applicable
+	 * @param item
+	 * @return boolean
 	 */
 	public static boolean isApplicable(ItemStack item) {
 		switch (item.getType()) {
@@ -59,175 +61,83 @@ public class Armor {
 			case LEATHER_HELMET:
 			case LEATHER_LEGGINGS:
 				return true;
-			default:
-				return false;
 		}
-	}
-	
-	/**
-	 * Sets the color.
-	 *
-	 * @param item item to color
-	 * @param color color
-	 * @return colored item
-	 * @throws Exception thrown,  when item is not applicable
-	 */
-	public static ItemStack setColor(ItemStack item, Color color) {
-		if (!isApplicable(item))
-			return null;
-		CraftItemStack craftStack = null;
-		net.minecraft.server.ItemStack itemStack = null;
-		if (item instanceof CraftItemStack) {
-			craftStack = (CraftItemStack) item;
-			itemStack = craftStack.getHandle();
-		}
-		else if (item instanceof ItemStack) {
-			craftStack = new CraftItemStack(item);
-			itemStack = craftStack.getHandle();
-		}
-		NBTTagCompound tag = itemStack.tag;
-		if (tag == null) {
-			tag = new NBTTagCompound();
-			tag.setCompound("display", new NBTTagCompound());
-			itemStack.tag = tag;
-		}
-		
-		tag = itemStack.tag.getCompound("display");
-		tag.setInt("color", color.getRGB());
-		itemStack.tag.setCompound("display", tag);
-		return craftStack;
+		return false;
 	}
 
 	/**
 	 * Sets the color.
+	 * If item is not colorable it will return an unchanged object of that item.
 	 *
-	 * @param item item to color
+	 * @param item  item to color
+	 * @param color color
+	 * @return colored item
+	 */
+	public static ItemStack setColor(ItemStack item, Color color) {
+		return setColor(item, color.getRGB());
+	}
+
+	/**
+	 * Sets the color.
+	 * If item is not colorable it will return an unchanged object of that item.
+	 *
+	 * @param item  item to color
 	 * @param color color
 	 * @return colored item
 	 */
 	public static ItemStack setColor(ItemStack item, ArmorColor color) {
-		if (!isApplicable(item))
-			return null;
-		CraftItemStack craftStack = null;
-		net.minecraft.server.ItemStack itemStack = null;
-		if (item instanceof CraftItemStack) {
-			craftStack = (CraftItemStack) item;
-			itemStack = craftStack.getHandle();
-		}
-		else if (item instanceof ItemStack) {
-			craftStack = new CraftItemStack(item);
-			itemStack = craftStack.getHandle();
-		}
-		NBTTagCompound tag = itemStack.tag;
-		if (tag == null) {
-			tag = new NBTTagCompound();
-			tag.setCompound("display", new NBTTagCompound());
-			itemStack.tag = tag;
-		}
-		
-		tag = itemStack.tag.getCompound("display");
-		tag.setInt("color", color.getColor());
-		itemStack.tag.setCompound("display", tag);
-		return craftStack;
+		return setColor(item, color.getColor());
 	}
 
 	/**
 	 * Sets the color.
+	 * If item is not colorable it will return an unchanged object of that item.
 	 *
-	 * @param item item to color
+	 * @param item     item to color
 	 * @param colorStr color
 	 * @return colored item
 	 */
 	public static ItemStack setColor(ItemStack item, String colorStr) {
-		if (!isApplicable(item))
-			return null;
-		int color = Integer.decode(colorStr);
-		CraftItemStack craftStack = null;
-		net.minecraft.server.ItemStack itemStack = null;
-		if (item instanceof CraftItemStack) {
-			craftStack = (CraftItemStack) item;
-			itemStack = craftStack.getHandle();
-		}
-		else if (item instanceof ItemStack) {
-			craftStack = new CraftItemStack(item);
-			itemStack = craftStack.getHandle();
-		}
-		NBTTagCompound tag = itemStack.tag;
-		if (tag == null) {
-			tag = new NBTTagCompound();
-			tag.setCompound("display", new NBTTagCompound());
-			itemStack.tag = tag;
-		}
-		
-		tag = itemStack.tag.getCompound("display");
-		tag.setInt("color", color);
-		itemStack.tag.setCompound("display", tag);
-		return craftStack;
+		return setColor(item, Integer.decode(colorStr));
 	}
-	
+
 	/**
 	 * Sets the color.
+	 * If item is not colorable it will return an unchanged object of that item.
 	 *
-	 * @param item item to color
+	 * @param item   item to color
 	 * @param colorR amount of red
 	 * @param colorG amount of green
 	 * @param colorB amount of blue
 	 * @return colored item
 	 */
 	public static ItemStack setColor(ItemStack item, int colorR, int colorG, int colorB) {
-		if (!isApplicable(item))
-			return null;
-		int color = Integer.decode(ColorConverter.toHex(colorR, colorG, colorB));
-		CraftItemStack craftStack = null;
-		net.minecraft.server.ItemStack itemStack = null;
-		if (item instanceof CraftItemStack) {
-			craftStack = (CraftItemStack) item;
-			itemStack = craftStack.getHandle();
-		}
-		else if (item instanceof ItemStack) {
-			craftStack = new CraftItemStack(item);
-			itemStack = craftStack.getHandle();
-		}
-		NBTTagCompound tag = itemStack.tag;
-		if (tag == null) {
-			tag = new NBTTagCompound();
-			tag.setCompound("display", new NBTTagCompound());
-			itemStack.tag = tag;
-		}
-		
-		tag = itemStack.tag.getCompound("display");
-		tag.setInt("color", color);
-		itemStack.tag.setCompound("display", tag);
-		return craftStack;
+		return setColor(item, Integer.decode(ColorConverter.toHex(colorR, colorG, colorB)));
 	}
-	
+
 	/**
 	 * Gets the color.
 	 *
 	 * @param item colored item
-	 * @return color
+	 * @return color or NULL if no color was found for given item
 	 */
-	public static int getColor(ItemStack item) {
+	public static Integer getColor(ItemStack item) {
+
 		if (!isApplicable(item))
-			return -1;
-		CraftItemStack craftStack = null;
-		net.minecraft.server.ItemStack itemStack = null;
-		if (item instanceof CraftItemStack) {
+			return null;
+
+		CraftItemStack craftStack;
+
+		if (item instanceof CraftItemStack){
 			craftStack = (CraftItemStack) item;
-			itemStack = craftStack.getHandle();
-		}
-		else if (item instanceof ItemStack) {
+		}else{
 			craftStack = new CraftItemStack(item);
-			itemStack = craftStack.getHandle();
 		}
+
+		net.minecraft.server.ItemStack itemStack = craftStack.getHandle();
 		NBTTagCompound tag = itemStack.tag;
-		if (tag == null) {
-			tag = new NBTTagCompound();
-			tag.setCompound("display", new NBTTagCompound());
-			itemStack.tag = tag;
-			return -1;
-		}
-		
+		if (tag == null) return null; // has no color
+
 		tag = itemStack.tag.getCompound("display");
 		return tag.getInt("color");
 	}
